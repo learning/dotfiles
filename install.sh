808b01f5206254d44b340e7b815ff9ab0a72ccf6
Yongning Liang <liangyongning@gmail.com>
2014-10-31 00:14:19 +0800

    add python.sublime-build

--- install.sh
+++ install.sh
@@ -2,16 +2,22 @@
 # link dotfiles to home directory
 
 # get current working directory
-cwd=$(cd "$(dirname "$0")"; pwd)
+CWD=$(cd "$(dirname "$0")"; pwd)
 
 # For zsh
-if [ ! -f "~/.zshrc" ]; then
-	rm ~/.zshrc
-fi
-ln -s $cwd/.zshrc ~/.zshrc
+rm ~/.zshrc
+ln -s $CWD/.zshrc ~/.zshrc
 
 # For vim
-if [ ! -f "~/.vimrc" ]; then
-	rm ~/.vimrc
-fi
-ln -s $cwd/.vimrc ~/.vimrc
+rm ~/.vimrc
+ln -s $CWD/.vimrc ~/.vimrc
+
+# For Sublime Text 3
+_IFS_=$IFS
+IFS="|"
+SUBLIME="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+rm $SUBLIME/Python.sublime-build
+ln -s "$CWD/Sublime Text 3/Python.sublime-build" $SUBLIME/Python.sublime-build
+rm $SUBLIME/Preferences.sublime-settings
+ln -s "$CWD/Sublime Text 3/Preferences.sublime-settings" $SUBLIME/Preferences.sublime-settings
+IFS=$_IFS_

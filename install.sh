a438df42201bdc25f864e5e1c7a8f7694515cd60
Learning <liangyongning@gmail.com>
2014-11-01 20:24:16 +0000

    remove tabs

--- install.sh
+++ install.sh
@@ -16,16 +16,16 @@ rm ~/.vimrc
 ln -s $CWD/.vimrc ~/.vimrc
 
 if [ $OS = "Darwin" ]; then
-	# For Sublime Text 3
-	_IFS_=$IFS
-	IFS="|"
-	SUBLIME="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-	rm $SUBLIME/Python.sublime-build
-	ln -s "$CWD/Sublime Text 3/Python.sublime-build" $SUBLIME/Python.sublime-build
-	rm $SUBLIME/Preferences.sublime-settings
-	ln -s "$CWD/Sublime Text 3/Preferences.sublime-settings" $SUBLIME/Preferences.sublime-settings
-	IFS=$_IFS_
+  # For Sublime Text 3
+  _IFS_=$IFS
+  IFS="|"
+  SUBLIME="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+  rm $SUBLIME/Python.sublime-build
+  ln -s "$CWD/Sublime Text 3/Python.sublime-build" $SUBLIME/Python.sublime-build
+  rm $SUBLIME/Preferences.sublime-settings
+  ln -s "$CWD/Sublime Text 3/Preferences.sublime-settings" $SUBLIME/Preferences.sublime-settings
+  IFS=$_IFS_
 else
-	echo "Linux no need Sublime Text 3"
+  echo "Linux no need Sublime Text 3"
 fi
 
